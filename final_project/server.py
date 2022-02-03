735c58041e841f4182be63aa6cdf9104f164bc27
gitbit789 <28954292+gitbit789@users.noreply.github.com>
2022-02-03 15:01:10 -0600

    update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,25 +1,28 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-import machinetranslation
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    
-    return "Translated text to French"
+    translatedText = translator.englishToFrench(textToTranslate);
+    return translatedText
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
+    translatedText = translator.frenchToEnglish(textToTranslate);
+    return translatedText
     
-    return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
+    return render_template('index.html')
     
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
+
